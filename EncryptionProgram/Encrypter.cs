3d81efbb823b66953e7d288abcac3bd19a9bd963
Muradov2004 <ami65143@gmail.com>
2023-06-28 20:58:33 +0400

    Updated

---
+++ EncryptionProgram/Encrypter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncryptionProgram;
+
+class Encrypter
+{
+    public int EncryptionKey { get; set; }
+
+    public char Encrypt(char Character)
+    {
+
+        Character |= (char)EncryptionKey;
+        return Character;
+    }
+}
--- EncryptionProgram/MainWindow.xaml.cs
+++ EncryptionProgram/MainWindow.xaml.cs
@@ -1,35 +1,51 @@
 ﻿using System.Windows;
 using Wpf.Ui.Controls;
 using Microsoft.Win32;
+using System.Text.RegularExpressions;
+using System.Windows.Input;
 
-namespace EncryptionProgram
+namespace EncryptionProgram;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : UiWindow
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : UiWindow
+    public MainWindow()
     {
-        public MainWindow()
+        InitializeComponent();
+    }
+
+    private void DialogButton_Click(object sender, RoutedEventArgs e)
+    {
+        var dialog = new OpenFileDialog
         {
-            InitializeComponent();
-        }
+            FileName = "Encryption file",
+            DefaultExt = ".txt",
+            Filter = "Text documents (.txt)|*.txt"
+        };
 
-        private void DialogButton_Click(object sender, RoutedEventArgs e)
+        bool? result = dialog.ShowDialog();
+
+        if (result == true)
         {
-            var dialog = new OpenFileDialog();
-            dialog.FileName = "Encryption file"; 
-            dialog.DefaultExt = ".txt";
-            dialog.Filter = "Text documents (.txt)|*.txt"; 
-
-            // Show open file dialog box
-            bool? result = dialog.ShowDialog();
-
-            // Process open file dialog box results
-            if (result == true)
-            {
-                // Open document
-                FilePathTxtBox.Text = dialog.FileName;
-            }
+            FilePathTxtBox.Text = dialog.FileName;
         }
     }
+
+    private void NumericPasswordBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+    {
+        if (!IsNumericInput(e.Text))
+            e.Handled = true;
+
+    }
+
+    private bool IsNumericInput(string input)
+    {
+        string pattern = "[^0-9]+";
+
+        return !Regex.IsMatch(input, pattern);
+    }
+
+
 }
